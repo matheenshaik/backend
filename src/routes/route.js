3c3f399abab340140bcbc179b98af86db3699bad
matheenshaik <pooja@LAPTOP-GH4T6A65>
2022-08-10 14:39:51 +0530

    completed

--- src/routes/route.js
+++ src/routes/route.js
@@ -63,11 +63,56 @@ router.post("/test-post-3", function(req, res) {
 
 
 
+
 router.post("/test-post-4", function(req, res) {
     let arr= [ 12, "functionup"]
     let ele= req.body.element
     arr.push(ele)
     res.send(  { msg: arr , status: true }  )
 })
+
+
+
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ],
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ],
+       },
+   ]
+
+   router.post('/players', function (req, res) {
+    let sport = req.body.player
+
+    players.push(sport)
+
+       res.send(  { data: players , status: true }  )
+   })
+  
+
+
 
 module.exports = router;
