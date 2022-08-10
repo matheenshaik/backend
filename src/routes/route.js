164b2239937eece603d33a33a7a71cb69b522d4a
matheenshaik <pooja@LAPTOP-GH4T6A65>
2022-08-10 22:11:23 +0530

    uplode

--- src/routes/route.js
+++ src/routes/route.js
@@ -64,6 +64,55 @@ router.post("/test-post-3", function(req, res) {
 
 
 
+// router.post("/test-post-4", function(req, res) {
+//     let arr= [ 12, "functionup"]
+//     let ele= req.body.element
+//     arr.push(ele)
+//     res.send(  { msg: arr , status: true }  )
+// })
+
+
+
+// let players =
+//    [
+//        {
+//            "name": "manish",
+//            "dob": "1/1/1995",
+//            "gender": "male",
+//            "city": "jalandhar",
+//            "sports": [
+//                "swimming"
+//            ]
+//        },
+//        {
+//            "name": "gopal",
+//            "dob": "1/09/1995",
+//            "gender": "male",
+//            "city": "delhi",
+//            "sports": [
+//                "soccer"
+//            ],
+//        },
+//        {
+//            "name": "lokesh",
+//            "dob": "1/1/1990",
+//            "gender": "male",
+//            "city": "mumbai",
+//            "sports": [
+//                "soccer"
+//            ],
+//        },
+//    ]
+
+
+
+//    router.post('/players', function (req, res) {
+//     let sport = req.body.player
+
+//     players.push(sport)
+
+//        res.send(  { data: players , status: true }  )
+//    })
 router.post("/test-post-4", function(req, res) {
     let arr= [ 12, "functionup"]
     let ele= req.body.element
@@ -105,11 +154,25 @@ let players =
    ]
 
    router.post('/players', function (req, res) {
-    let sport = req.body.player
+    
+   
+    let sport = req.body.player.name
+    let isNameRepeated= false
 
-    players.push(sport)
+    for (let i=0; i<players.length; i++){
+        if (players[i].name == sport){
+            isNameRepeated= true
+            break
+        } 
+    }
+        
+        if (isNameRepeated = true){
+            res.send('player already in list')
 
-       res.send(  { data: players , status: true }  )
+        } else{
+            players.push(sport)
+       res.send(players)
+        }
    })
   
 
