8190c0e866a3ceb0eb494e1726e9f363e44f839e
matheenshaik <pooja@LAPTOP-GH4T6A65>
2022-08-18 14:26:33 +0530

    session3 commplete

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
-    useNewUrlParser: true
-})
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+
+mongoose.connect("mongodb+srv://matheenahamad:9TNGWEhzUB0Ttemi@matheen.vtdepfw.mongodb.net/matheenahamad?retryWrites=true&w=majority", {useNewUrlParser: true})
+.then( () =>console.log("MongoDb is connected"))
+.catch ( err =>console.log(err))
+
 
 app.use('/', route);
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -4,38 +4,54 @@ const router = express.Router();
 const UserController= require("../controllers/userController")
 const BookController= require("../controllers/bookController")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+const bookModel = require('../models/bookModel')
+const authorModel = require('../models/authorsModel.js')
+const about =  require('../controllers/CRUD')
 
-router.post("/createUser", UserController.createUser  )
+router.post('/books', about.BookIn)
 
-router.get("/getUsersData", UserController.getUsersData)
+router.post('/author', about.Author)
 
-router.post("/createBook", BookController.createBook  )
+router.get('/update', about.update)
 
-router.get("/getBooksData", BookController.getBooksData)
+router.get('/price', about.price)
 
-router.post("/updateBooks", BookController.updateBooks)
-router.post("/deleteBooks", BookController.deleteBooks)
+router.get('/range', about.range)
+
+
+
+
+
+
+
+// router.post("/createUser", UserController.createUser  )
+
+// router.get("/getUsersData", UserController.getUsersData)
+
+// router.post("/createBook", BookController.createBook  )
+
+// router.get("/getBooksData", BookController.getBooksData)
+
+// router.post("/updateBooks", BookController.updateBooks)
+// router.post("/deleteBooks", BookController.deleteBooks)
 
 //MOMENT JS
-const moment = require('moment');
-router.get("/dateManipulations", function (req, res) {
+// const moment = require('moment');
+// router.get("/dateManipulations", function (req, res) {
     
-    // const today = moment();
-    // let x= today.add(10, "days")
+//     // const today = moment();
+//     // let x= today.add(10, "days")
 
-    // let validOrNot= moment("29-02-1991", "DD-MM-YYYY").isValid()
-    // console.log(validOrNot)
+//     // let validOrNot= moment("29-02-1991", "DD-MM-YYYY").isValid()
+//     // console.log(validOrNot)
     
-    const dateA = moment('01-01-1900', 'DD-MM-YYYY');
-    const dateB = moment('01-01-2000', 'DD-MM-YYYY');
+//     const dateA = moment('01-01-1900', 'DD-MM-YYYY');
+//     const dateB = moment('01-01-2000', 'DD-MM-YYYY');
 
-    let x= dateB.diff(dateA, "days")
-    console.log(x)
+//     let x= dateB.diff(dateA, "days")
+//     console.log(x)
 
-    res.send({ msg: "all good"})
-})
+//     res.send({ msg: "all good"})
+// })
 
 module.exports = router;
